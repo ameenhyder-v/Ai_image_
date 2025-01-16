fe76fa8cb14bf3aaaea8e68a373269d889adfec1
ameenhyder-v <ameenhyder.v@gmail.com>
2025-01-16 16:04:54 +0530

    changed the layout file

--- app/layout.tsx
+++ app/layout.tsx
@@ -2,7 +2,6 @@ import type { Metadata } from "next";
 import { Roboto } from "next/font/google";
 import "./globals.css";
 import ResponsiveNav from "@/components/Home/Navbar/ResponsiveNav";
-import Home from "@/components/Home/Home";
 import { Toaster } from "@/components/ui/sonner";
 import Footer from "@/components/Home/Footer/Footer";
 
